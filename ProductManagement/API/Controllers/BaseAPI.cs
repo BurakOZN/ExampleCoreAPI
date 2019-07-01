74a0cd1264a5855b5ff82023dad0739a99edb0df
BurakOZN <burakozn@gmail.com>
2019-07-01 18:04:56 +0300

    Ekleme: Base API class'ı eklendi. Temel CRUD işlemleri misar alan APIController'lar için burada yapılacak.

---
+++ ProductManagement/API/Controllers/BaseAPI.cs
@@ -0,0 +1,76 @@
+﻿using BLL;
+using Entity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class BaseAPI<T> : ControllerBase where T : BaseEntity
+    {
+        private IRepository<T> _repository;
+        public BaseAPI(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string id)
+        {
+            T entity = _repository.Get(x => x.Id == id).FirstOrDefault();
+            return Ok(entity);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<T> subWork = _repository.Get();
+            return Ok(subWork);
+        }
+
+        [HttpPost]
+        public IActionResult Add(T entity)
+        {
+            CrudState state = _repository.Add(entity);
+            if (state == CrudState.Success)
+                return Ok(entity);
+            else
+                return Ok(null);//hata durumu dönebiliriz düşünelim
+        }
+
+        [HttpPost]
+        public IActionResult AddRange(List<T> entity)
+        {
+            CrudState state = _repository.Add(entity);
+            if (state == CrudState.Success)
+                return Ok(entity);
+            else
+                return Ok(null);//hata durumu dönebiliriz düşünelim
+        }
+
+
+        [HttpPut]
+        public IActionResult Update(T entity) // updaterange yapılabilir.
+        {
+            CrudState state = _repository.Update(entity);
+            if (state == CrudState.Success)
+                return Ok(entity);
+            else
+                return Ok(null);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string id)
+        {
+            CrudState state = _repository.Delete(id);
+            if (state == CrudState.Success)
+                return Ok(true);
+            else
+                return Ok(false);
+        }
+
+    }
+}
