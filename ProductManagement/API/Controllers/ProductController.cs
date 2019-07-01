60e925b34e6af4276858685c1e8b0b733b79aa1c
BurakOZN <burakozn@gmail.com>
2019-07-01 22:05:20 +0300

    Ekleme: Entityler için controllerlar eklendi.

---
+++ ProductManagement/API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL;
+using Entity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]/[action]"), Authorize]
+    public class ProductController : BaseAPI<Product>
+    {
+        IRepository<Product> _repository;
+        private readonly ILogger<ProductController> _logger;
+        public ProductController(IRepository<Product> repository, ILogger<ProductController> logger) : base(repository)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public IActionResult UserProducts(string userid)
+        {
+            var products = _repository.Get(x => x.UserId == userid);
+            return Ok(products);
+        }
+    }
+}
---
+++ ProductManagement/API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL;
+using Entity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [Authorize]
+    public class UserController : BaseAPI<User>
+    {
+        IRepository<User> _repository;
+        private readonly ILogger<UserController> _logger;
+        public UserController(IRepository<User> repository, ILogger<UserController> logger) : base(repository)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+    }
+}
