00e86e87d0d23c82112bf356a73c278ac782c634
BurakOZN <burakozn@gmail.com>
2019-07-01 18:00:17 +0300

    Düzenleme: İlişkiler kuruldu.

--- ProductManagement/DAL/ManContext.cs
+++ ProductManagement/DAL/ManContext.cs
@@ -1,8 +1,38 @@
-﻿using System;
+﻿using Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace DAL
 {
-    public class Class1
+    public class ManContext : DbContext
     {
+        public ManContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            //Primary Key Column
+            mb.Entity<User>()
+                .HasKey(x => x.Id);
+            mb.Entity<Product>()
+                .HasKey(x => x.Id);
+
+            //Index Column
+            mb.Entity<User>()
+                .HasIndex(x => x.Email);
+            
+
+            //FK
+            mb.Entity<Product>()
+                .HasOne(x => x.User)
+                .WithMany(x => x.Products)
+                .HasForeignKey(x => x.UserId);
+
+            base.OnModelCreating(mb);
+        }
     }
 }
