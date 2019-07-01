575e80e4b0c8a8e8428f1675be9c4050cef3fac0
BurakOZN <burakozn@gmail.com>
2019-07-01 22:03:01 +0300

    Ekleme: JWT token oluşturma eklendi.

---
+++ ProductManagement/API/Controllers/TokenController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+using Entity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        IRepository<User> _repository;
+        public TokenController(IRepository<User> repository)
+        {
+            _repository = repository;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("token")]
+        public IActionResult Post([FromBody]LoginRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _repository.Get(x => x.Email == request.UserName && x.Password == request.Password).FirstOrDefault();
+                if (user == null)
+                {
+                    return Unauthorized();
+                }
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, request.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.Role,"Admin")
+                };
+
+                var token = new JwtSecurityToken
+                (
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddHours(3), // 3 saat geçerli olacak
+                    notBefore: DateTime.UtcNow,
+                    signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("990601af-b3cd-4c09-803a-d39d998b92af")),//appsettings.json içerisinde bulunan signingkey değeri
+                            SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+            }
+            return BadRequest();
+        }
+
+        public class LoginRequest
+        {
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
